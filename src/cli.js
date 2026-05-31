f5e104433e3929555074779cf680fa1c0181a3ec
Sergey <mitugov@bk.ru>
2026-05-31 14:29:27 +0300

    Fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -4,5 +4,5 @@ export const run = () => {
     console.log('Welcome to the Brain Games!');
 
     const userName = readlineSync.question('May I have your name? ');
-    console.log('Hello ' + userName + '!');
+    console.log('Hello, ' + userName + '!');
 }
