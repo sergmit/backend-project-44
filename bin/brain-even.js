d3577969fe8c0768710ef3a3d7fb0dfddaec4648
Sergey <mitugov@bk.ru>
2026-05-31 12:08:17 +0300

    Brain even game refactoring

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,35 +1,7 @@
 #!/usr/bin/env node
 import readlineSync from "readline-sync";
+import BrainEvenApp from "../src/BrainEvenApp.js";
 
-const run = () => {
-    console.log('Welcome to the Brain Games!');
+const app = new BrainEvenApp()
 
-    const userName = readlineSync.question('May I have your name? ');
-    console.log('Hello ' + userName + '!');
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-    const countAttempts = 3;
-    let isWin = false;
-
-    for (let i = 1; i <= countAttempts; i++) {
-        const number = Math.round(Math.random() * 100);
-        const isEven = number % 2 === 0;
-        console.log(`Question: ${number}`);
-        const answer = readlineSync.question('Your answer: ').toLowerCase();
-        const correctAnswer = isEven ? 'yes' : 'no'
-        if (answer !== correctAnswer) {
-            console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`)
-            console.log(`Let's try again, ${userName}!`);
-            isWin = false;
-            break;
-        }
-        console.log('Correct!');
-        isWin = true;
-    }
-    if (isWin) {
-        console.log(`Congratulations, ${userName}!`);
-    }
-
-}
-
-run();
+app.start();
---
+++ src/BrainEvenApp.js
@@ -0,0 +1,41 @@
+import readlineSync from "readline-sync";
+
+class BrainEvenApp {
+
+    constructor() {
+        this.countAttempts = 3
+    }
+
+    start() {
+        console.log('Welcome to the Brain Games!');
+
+        const userName = readlineSync.question('May I have your name? ');
+        console.log('Hello ' + userName + '!');
+        console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+        const countAttempts = 3;
+        let isWin = false;
+
+        for (let i = 1; i <= this.countAttempts; i++) {
+            const number = Math.round(Math.random() * 100);
+            const isEven = number % 2 === 0;
+            console.log(`Question: ${number}`);
+            const answer = readlineSync.question('Your answer: ').toLowerCase();
+            const correctAnswer = isEven ? 'yes' : 'no'
+            if (answer !== correctAnswer) {
+                console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`)
+                console.log(`Let's try again, ${userName}!`);
+                isWin = false;
+                break;
+            }
+            console.log('Correct!');
+            isWin = true;
+        }
+        if (isWin) {
+            console.log(`Congratulations, ${userName}!`);
+        }
+
+    }
+}
+
+export default BrainEvenApp;
