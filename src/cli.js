acd4d0a8ed3a0095869f45848b505463923ef09a
Sergey <mitugov@bk.ru>
2026-06-02 18:03:24 +0300

    Brain game refactoring

--- src/cli.js
+++ src/cli.js
@@ -101,7 +101,7 @@ const generateProgressionCollection = () => {
     const length = Math.floor(Math.random() * 6) + 5
     let start = Math.floor(Math.random() * 50) + 1
     const step = Math.floor(Math.random() * 5) + 1
-    const numbers = [step]
+    const numbers = [start]
     for (let i = 1; i < length; i++) {
       start += step
       numbers.push(start)
